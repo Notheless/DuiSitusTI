28b73cd47d65e3eb390ed670295a8087eeb9438b
rionardians <rioriogue@gmail.com>
2017-10-31 18:04:41 +0700

    Add files via upload

---
+++ organisasi.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Organisasi dan Tata Pamong</title>  
+	<?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+    <div class="grid-container">
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+		
+        <div class="grid-headline-header">
+            <h1 class="headline-header"> ORGANISASI <span class="header-revcolor">DAN TATA PAMONG </span></h1>
+        </div>
+        <div class="grid-headline-para">        
+            <p class="headline-para">
+                Sistem tata pamong di Prodi Teknik Informatika FMIPA Unpad, yang mengacu kepada sistem Organisasi dan Tata Kelola (OTK) Universitas Padjadjaran, terdiri dari tiga komponen yaitu tenaga pendidik, tenaga kependidikan dan mahasiswa.
+				Selain itu komponen lainnya yaitu alumni dan pengguna alumni juga terlibat dalam pengembangan Prodi. 
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <img style="thumbnail" src="f_img/03.jpg" height="100%px" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2></h2>
+                <p>An organization, no matter how well designed, is only as good as the people who live and work in it.
+				<br> <div style="text-align:right"> - Dee Hock</div>
+				</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+                <p>
+                    Selain itu komponen lainnya yaitu alumni dan pengguna alumni juga terlibat dalam pengembangan Prodi. Prodi Teknik Informatika dipimpin oleh seorang Ketua Prodi dibantu oleh seorang Sekretaris Prodi. Ketua dan Sekretaris Prodi dipilih dari dosen Prodi melalui mekanisme aturan pemilihan yang mengacu pada peraturan dan undang-undang yang berlaku. Calon yang akan dipilih berasal dari staf dosen yang telah memenuhi persyaratan baik dari kualifikasi akademik, jabatan, kepangkatan serta syarat non akademik seperti memiliki sifat kepemimpinan dan tanggung jawab. Calon terpilih diusulkan oleh Prodi ke Fakultas kemudian ke Universitas dalam rangka penetapan Ketua dan Sekretaris Prodi oleh Rektor. Uraian lebih jauh tentang tata pamong, susunan organisisi dan lain sebagainya dapat dilihat disini.
+                </p>
+                <h1>Umpan Balik</h1>
+                <p>
+                    <a href>Umpan balik dari Dosen -> Institusi</a><br>
+                    <a href>Umpan bali dari Tenaga Kependidikan -> Institusi</a><br>
+                    <a href>Umpan balik dari Masyarakat</a>
+                </p>
+                <h1>Pemantauan dan Evaluasi (Monev)</h1>
+                <p>
+                    Sistem Akreditasi Nasional Perguruan Tinggi oleh BAN-PT<br>
+                    Sistem Penjaminan Mutu Universitas oleh <a href>SPM Unpad</a><br>
+                    Monitoring dan Evaluasi Internal oleh Fakultas<br>
+                    Evaluasi Laporan Kinerja Dosen oleh Tim Sertifikasi Dosen<br>
+                    Evaluasi Pelaksanaan Pengajaran oleh Program Studi
+                </p>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ visi_misi.php
@@ -0,0 +1,101 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Visi dan Misi</title>  
+	<?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+    <!-- container -->
+    <div class="grid-container">
+        
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+		<div class="grid-headline-header">
+            <h1 class="headline-header"> VISI <span class="header-revcolor">DAN MISI </span></h1>
+        </div>
+        <div class="grid-headline-para">
+            <p class="headline-para">
+                Visi adalah pandangan jauh tentang suatu perusahaan ataupun lembaga, dapat di artikan sebagai tujuan dan apa yang harus dilakukan untuk mencapai tujuannya tersebut.
+				<br><br>Sedangkan Misi adalah suatu pernyataan tentang apa yang harus dikerjakan oleh perusahaan atau lembaga dalam usaha mewujudkan Visi.
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <span class="center-im"></span>
+            <img class="thumbnail" src="f_img/02.jpg" height="auto" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2></h2>
+                <p>Vision without action is merely a dream. Action without vision just passed the time. Vision with action can change the world.
+					<br> <div style="text-align:right"> - Joel A. Barker</div>
+				</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+                <h1>Visi</h1>
+                <p>
+                    Pada tahun 2026, menjadi Prodi Teknik Informatika yang unggul di tingkat nasional serta diakui di tingkat regional dalam bidang pendidikan, penelitian, dan pengabdian kepada masyarakat.
+                </p>
+                <h1>Misi</h1>
+                <p>
+                    <ol>
+                        <li>Menghasilkan lulusan yang bermutu, bermoral, dan profesional dalam bidang teknik informatika serta mampu beradaptasi terhadap perkembangan ilmu pengetahuan dan teknologi sehingga dapat bersaing secara nasional dan regional.</li>
+                        <li>Meningkatkan pemberdayaan kelompok penelitian/kelompok bidang Minat (KBM) dalam pengembangan dan penerapan bidang sistem informasi dan rekayasa perangkat lunak, jaringan komputer dan komunikasi data, sistem cerdas dan sistem grafika, dan ilmu komputasi dan metode numerik. yang bertaraf nasional dan regional, untuk mendukung pengembangan ilmu pengetahuan dan teknologi.</li>
+                        <li>Meningkatkan penerapan hasil penelitian dasar dan terapan dalam bidang informatika untuk ikut serta dalam mendukung pembangunan nasional dan kesejahteraan masyarakat.</li>
+                    </ol>
+                </p>
+                <h1>Sasaran</h1>
+                <h1>Tujuan</h1>
+                <h1>Rencana Strategis</h1>
+                <p>
+                    <ol>
+                        <li><a href>Rencana Strategis UNPAD 2015-2019</a></li>
+                        <li><a href>Rencana Strategis Fakultas MIPA 2017-2021</a></li>
+                    </ol>
+                </p>
+                <h1>Latar Belakang</h1>
+                <p>
+                    Program Sarjana Program Studi Teknik Informatika merupakan pengembangan dari Bidang Minat Ilmu Komputer di Jurusan Matematika FMIPA Universitas Padjadjaran, sesuai dengan rencana strategis jurusan, fakultas, dan universitas. Kurikulum Bidang Minat Ilmu Komputer diberlakukan tahun 2004 dan telah menghasilkan lulusan yang terserap di berbagai institusi baik negeri maupun swasta tersebar di Indonesia.
+                </p>
+                <p>
+                    Mengingat pertumbuhan teknologi yang cepat dan tingginya kebutuhan masyarakat dalam bidang Teknik Informatika (lulusan sekitar 5% dari kebutuhan), dan tingginya minat untuk mengikuti pendidikan bidang Teknik Informatika (sekitar 4% diterima dari peminat SNMPTN), serta mempertimbangkan berbagai masukan dari stakeholder dan alumni untuk menyempurnakan kurikulum yang mandiri sesuai dengan kebutuhan, maka disusun proposal pendirian Program Studi Teknik Informatika, dan telah mendapat persetujuan Dikti dengan
+                    <a href><b>SK Mendikbud Nomor: 305/E/O/2011</b></a> tanggal 21 Desember 2011 tentang Ijin Penyelenggaraan Program Studi Teknik Informatika pada Universitas Padjadjaran. Pada tahun 2014 program studi S-1 Teknik Informatika memperoleh akreditasi B dari BAN PT sesuai dengan
+                    <a href><b>Surat Keputusan Nomor 483/SK/BAN-PT/Akred/S/XII/2014</b></a>.
+                </p>
+                <h1>Kompetensi Lulusan</h1>
+                <p>
+                    Lulusan Program Studi Teknik Informatika Universitas Padjadjaran bergelar Sarjana Teknik (S.Kom.,/S.Inf.), memiliki kompetensi sebagai berikut:
+                </p>
+                <p>
+                    <b>KOMPETENSI UTAMA:</b> mampu melakukan pengembangan perangkat lunak untuk berbagai keperluan, melakukan analisis terhadap sistem dalam suatu instansi atau perusahaan dan membuat solusi yang integratif dengan memanfaatkan perangkat lunak, mampu merencanakan dan pengevaluasian penerapan Teknologi Informasi pada sebuah organisasi, mampu merancang dan memelihara basis data termasuk data warehouse untuk suatu instansi atau perusahaan, mampu melakukan pengujian fungsi-fungsi perangkat lunak, mampu mengembangkan, merancang dan membangun serta memelihara website dengan berbagai layanan dan fasilitasnya.
+                </p>
+                <p>
+                    <b>KOMPETENSI KHUSUS:</b> lulusan teknik Informatika Unpad dapat memilih salah satu dari empat peminatan berikut:
+                    <br>(1) Sistem Informasi dan Rekayasa Perangkat Lunak (SIRPL),
+                    <br>(2) Jaringan Komputer dan Komunikasi Data (JKKD),
+                    <br>(3) Sistem Cerdas dan Sistem Grafika (SCSG), serta
+                    <br>(4) Ilmu Komputasi dan Metode Numerik (IKMN).
+                </p>
+                <h1>Prospek Lulusan</h1>
+                <p>
+                    <ul>
+                        <li>Lulusan program studi Teknik Informatika Universitas Padjadjaran diharapkan dapat mengisi lapangan kerja di berbagai institusi pemerintah, perusahaan swasta, dan industri.</li>
+                        <li>Lulusan Teknik Informatika dapat menjadi karyawan, wiraswastawan (entreupreneur), profesional, birokrat, dan akademisi.</li>
+                        <li>Lulusan diarahkan dapat mengisi karir pekerjaan bidang supervisi, asisten manajer dan administrator.</li>
+                        <li>Sebagai contoh lulusan Teknik Informatika harus mampu menjadi Capacity Planning Supervisor, Database Administrator, Customer Service Supervisor, Data Communication Assistant manager, Data Entry Supervisor, Information Center Manager, Hardware Installation Supervisor, System Administrator, Webmaster, Project Manager Applications, Project Manager Distributed Systems, Project Network Technical Services, Project Manager Implementation Deployment, dan lain-lain.</li>
+                    </ul>
+                </p>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
