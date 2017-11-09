ccdbc712f48bb778b9b66824805edbdfe7b32ed2
Novtrianda <32587933+Novtrianda@users.noreply.github.com>
2017-11-09 14:36:37 +0700

    update a1

---
+++ dosen.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Fasilitas dan Sistem Informasi</title>
+    <?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+	<div class="grid-dosen-container">
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+        <!-- konten -->
+        <div class="grid-dosen-header">
+            <h1 class="headline-header"> FASILITAS <span class="header-revcolor">DAN SISTEM INFORMASI </span></h1>
+        </div>
+        <div class="grid-dosen-info">
+            <div class="dosen-info">
+                <h1>Aditya Pradana, M.Eng</h1>
+                <div></div>
+                <p>Networking, data comm</p>
+            </div>
+        </div>
+        <div class="grid-dosen-pic">
+            <div class="photo-border">a</div>
+        </div>
+        <div class="grid-dosen-stats">
+            <div class="dosen-stats">
+                <p>Nama : </p>
+                <p>Email : </p>
+                <p>Telp : </p>
+                <p>HP : </p>
+                <p>Alamat : </p>
+                <p>S1 : </p>
+                <p>S2 : </p>
+            </div>
+        </div>
+        <div class="grid-dosen-data">
+            a
+        </div>
+        <div class="grid-dosen-list">a</div>
+    </div>
+</body>
+</html>
