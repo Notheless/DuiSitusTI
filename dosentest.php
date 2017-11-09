5a2b7c9387e65356211b3f6d7b1523f204ec55c9
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-09 14:30:33 +0700

    .

--- dosentest.php
+++ dosentest.php
@@ -8,32 +8,22 @@
 		echo "<th>ID</th>";
 		echo "<th>Nama</th>";
 		echo "<th>Email</th>";
+		echo "<th>Telp</th>";
+		echo "<th>HP</th>";
+		echo "<th>Alamat</th>";
+		echo "<th>Pendidikan</th>";
+		echo "<th>Bidang</th>";
 		while($row = $result->fetch_assoc()) 
 		{
 			echo "<tr>";
 			echo "<td>".$row["ID"]."</td>";
 			echo "<td>".$row["Nama"]."</td>";
 			echo "<td>".$row["Email"]."</td>";
-			echo "</tr>";
-		}
-		echo "</table>";
-	} 
-	else 
-	{
-		echo "0 results";
-	}
-	if ($result->num_rows > 0) 
-	{
-		echo "<table>";
-		echo "<th>ID</th>";
-		echo "<th>Nama</th>";
-		echo "<th>Email</th>";
-		while($row = $result->fetch_assoc()) 
-		{
-			echo "<tr>";
-			echo "<td>".$row["ID"]."</td>";
-			echo "<td>".$row["Nama"]."</td>";
-			echo "<td>".$row["Email"]."</td>";
+			echo "<td>".$row["Telp"]."</td>";
+			echo "<td>".$row["HP"]."</td>";
+			echo "<td>".$row["Alamat"]."</td>";
+			echo "<td>".$row["Pendidikan"]."</td>";
+			echo "<td>".$row["Bidang"]."</td>";
 			echo "</tr>";
 		}
 		echo "</table>";
