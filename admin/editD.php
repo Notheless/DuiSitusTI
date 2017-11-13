9ae5e89465617a7f3b86c4e788d3801693f0b5f1
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-13 17:32:20 +0700

    .

--- admin/editD.php
+++ admin/editD.php
@@ -94,55 +94,6 @@
                 </table>
             </form>
     </div>
-
-        <!-- insert -->
-        <!-- <div class="admin-content"> 
-            <h2 class="content-title">Input Dosen</h2>
-            <form action="test.php" method="post">
-                <table>
-                    <tr>
-                        <td><p>Judul Artikel : </p></td>
-                        <td width="70%"><input class="data-input" type="text" name="judul_artikel" placeholder="judul artikel"></td>
-                    </tr>
-                    <tr>
-                        <td><p>Tanggal : </p></td>
-                        <td><input class="data-input" type="datetime-local" name="tanggal_artikel" placeholder="11/7/2017" ></td>
-                    </tr>
-                    <tr>
-                        <td><p>Isi Artikel: </p></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><textarea rows="3" name="isi_artikel" placeholder="isi artikel"></textarea></td>
-                    </tr>
-                        <td></td><td style="text-align: right"><input class="button" type="submit" value="Masukkan"></td>
-                </table>
-            </form>
-        </div> -->
-
-        <!-- update -->
-        <!--  -->
-
-        <!-- delete -->
-        <!-- <div class="admin-content"> 
-            <h2 class="content-title">Hapus Artikel</h2>
-            <form action="" method="post">
-                <table style="text-align:center">
-                    <tr>
-                        <td><p>Artikel dengan judul : </p></td>
-                    </tr>
-                    <tr>
-                        <td><p>- ... -</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>Akan dihapus dan tidak bisa dikembalikan, apakah anda yakin? </p></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" style="display:inline-block; margin-top: 20px;"><p style="display:inline">Saya yakin dengan apa yang saya lakukan</p></td>
-                    </tr>
-                        <td><input class="button" type="submit" value="Hapus"></td>
-                </table>
-            </form>
-        </div> -->
     </div>
     <script type="text/javascript" src="js/textarea_dosen.js"></script>
     <script type="text/javascript" src="js/viedDosen.js"></script>
