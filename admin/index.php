ce4560f0201e0569ce4c0f6f7e285dc4127dbbd2
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-13 17:36:54 +0700

    the ting goes

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+The ting goes skrrrahh!<br>
+Pap, pap, ka-ka-ka!<br>
+Skidiki-pap-pap!<br>
+And a pu-pu-drrrr-boom!<br>
+Skya!<br>
+Du-du-ku-ku-pun-pun!<br>
+Poom, poom<br>
+You dun know, Big Shaq<br>
