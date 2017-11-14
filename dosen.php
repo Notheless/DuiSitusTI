d7bb03227d2437a5af2cde85f9768944bcf837bc
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-14 11:32:37 +0700

    .

--- dosen.php
+++ dosen.php
@@ -3,7 +3,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>Fasilitas dan Sistem Informasi</title>
+    <title>Tenaga Pendidik</title>
     <?php include 'header.html';?>
 </head>
 <body>
@@ -13,21 +13,14 @@
 			<?php include 'left_nav.html';?>
         <!-- konten -->
         <div class="grid-dosen-header">
-            <h1 class="headline-header"> DAFTAR <span class="header-revcolor">DOSEN </span></h1>
+            <h1 class="headline-header">TENAGA <span class="header-revcolor">PENDIDIK</span></h1>
         </div>
 		<?php
-		error_reporting(0);
 		include 'database.php';
 		$sql = "SELECT * FROM dosen";
 		$result = $conn->query($sql);
-		$id = 0;
-		if($_GET["ID"]) 
-		{
-			$id=$_GET["ID"];
-		}			
-		if(!$id==0){
 		while($row = $result->fetch_assoc()) {
-		if($row["ID"]==$id){
+		if($row["ID"]==$_GET["ID"]){
         echo "<div class=\"grid-dosen-info\">";
         echo    "<div class=\"dosen-info\">";
         echo        "<h1>".$row["Nama"]."</h1>";
@@ -40,22 +33,19 @@
         echo"</div>";
         echo"<div class=\"grid-dosen-stats\">";
         echo    "<div class=\"dosen-stats\">";
-        echo        "<p>Nama : ".$row["Nama"]."</p>";
-        echo        "<p>Email : ".$row["Email"]."</p>";
-        echo        "<p>Telp : ".$row["Telp"]."</p>";
-        echo        "<p>HP : ".$row["HP"]."</p>";
-        echo        "<p>Alamat : ".$row["Alamat"]."</p>";
-        echo        "<p>Pendidikan : ".$row["Pendidikan"]."</p>";
+		echo "<table style=\"width:100%;\">";
+		echo "<tr><td width=\"110px\"><p>Nama</p></td><td><p>:</p></td><td><p>".$row["Nama"]."</p></td></tr>";
+		echo "<tr><td width=\"110px\"><p>Email</p></td><td><p>:</p></td><td><p>".$row["Email"]."</p></td></tr>";
+		echo "<tr><td width=\"110px\"><p>Telp</p></td><td><p>:</p></td><td><p>".$row["Telp"]."</p></td></tr>";
+		echo "<tr><td width=\"110px\"><p>HP</p></td><td><p>:</p></td><td><p>".$row["HP"]."</p></td></tr>";
+		echo "<tr><td width=\"110px\"><p>Alamat</p></td><td><p>:</p></td><td><p>".$row["Alamat"]."</p></td></tr>";
+		echo "<tr><td width=\"110px\"><p>Pendidikan</p></td><td><p>:</p></td><td><p>".$row["Pendidikan"]."</td></tr>";
         echo    "</div>";
         echo"</div";
 		}
 		}
-		}
 		?>
-        <div class="grid-dosen-data">
-            a
-        </div>
-        <div class="grid-dosen-list">a</div>
     </div>
+	
 </body>
 </html>
