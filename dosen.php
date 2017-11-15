0bfaa52b99c414f783c204f6b6f4e0435391903f
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-15 20:29:16 +0700

    .

--- dosen.php
+++ dosen.php
@@ -19,32 +19,49 @@
 		include 'database.php';
 		$sql = "SELECT * FROM dosen";
 		$result = $conn->query($sql);
+		$id = $_GET["ID"];
 		while($row = $result->fetch_assoc()) {
-		if($row["ID"]==$_GET["ID"]){
-        echo "<div class=\"grid-dosen-info\">";
-        echo    "<div class=\"dosen-info\">";
-        echo        "<h1>".$row["Nama"]."</h1>";
-        echo        "<div></div>";
-        echo        "<p>".$row["Bidang"]."</p>";
-        echo    "</div>";
-        echo"</div>";
-        echo"<div class=\"grid-dosen-pic\">";
-        echo    "<div class=\"photo-border\"><img src=\"img/"."sam".".png\"></div>";
-        echo"</div>";
-        echo"<div class=\"grid-dosen-stats\">";
-        echo    "<div class=\"dosen-stats\">";
-		echo "<table style=\"width:100%;\">";
-		echo "<tr><td width=\"110px\"><p>Nama</p></td><td><p>:</p></td><td><p>".$row["Nama"]."</p></td></tr>";
-		echo "<tr><td width=\"110px\"><p>Email</p></td><td><p>:</p></td><td><p>".$row["Email"]."</p></td></tr>";
-		echo "<tr><td width=\"110px\"><p>Telp</p></td><td><p>:</p></td><td><p>".$row["Telp"]."</p></td></tr>";
-		echo "<tr><td width=\"110px\"><p>HP</p></td><td><p>:</p></td><td><p>".$row["HP"]."</p></td></tr>";
-		echo "<tr><td width=\"110px\"><p>Alamat</p></td><td><p>:</p></td><td><p>".$row["Alamat"]."</p></td></tr>";
-		echo "<tr><td width=\"110px\"><p>Pendidikan</p></td><td><p>:</p></td><td><p>".$row["Pendidikan"]."</td></tr>";
-        echo    "</div>";
-        echo"</div";
-		}
-		}
+		if($row["ID"]==$id){
         ?>
+		<div class="grid-dosen-info">
+			<div class="dosen-info">
+                <h1><?php echo$row["Nama"];?></h1>
+                <div></div>
+                <p><?php echo $row["Bidang"]; ?></p>
+			</div>
+		</div>
+        <div class="grid-dosen-pic">
+        <div class="photo-border">
+			<img src="img/sam.png"></div>
+		</div>
+        <div class="grid-dosen-stats">
+			<div class="dosen-stats">
+				<table style="width:100%;">
+					<tr><td width="110px"><p>Nama</p></td><td><p>:</p></td><td><p><?php echo $row["Nama"];?></p></td></tr>
+					<tr><td width="110px"><p>Email</p></td><td><p>:</p></td><td><p><?php echo $row["Email"];?></p></td></tr>
+					<tr><td width="110px"><p>Telp</p></td><td><p>:</p></td><td><p><?php echo $row["Telp"];?></p></td></tr>
+					<tr><td width="110px"><p>HP</p></td><td><p>:</p></td><td><p><?php echo $row["HP"];?></p></td></tr>
+					<tr><td width="110px"><p>Alamat</p></td><td><p>:</p></td><td><p><?php echo $row["Alamat"];?></p></td></tr>
+					<tr><td width="110px"><p>Pendidikan</p></td><td><p>:</p></td><td><p><?php echo $row["Pendidikan"];?></td></tr>
+				</table>
+        	</div>
+        </div>
+		<?php }} 
+			$sql = "SELECT * FROM detaildosen";
+			$result = $conn->query($sql);
+			$pengajaran='';
+			$penelitian='';
+			$pengabdian='';
+			$penunjang='';
+			while($row = $result->fetch_assoc()) {
+				if($row["IDDosen"]==$id){
+					if($row["IDCatagory"]==3) $pengajaran .= "<tr><td>".$row["Isi"]."</tr></td>";
+					if($row["IDCatagory"]==1) $penelitian .= "<tr><td>".$row["Isi"]."</tr></td>";
+					if($row["IDCatagory"]==2) $pengabdian .= "<tr><td>".$row["Isi"]."</tr></td>";
+					if($row["IDCatagory"]==4) $penunjang .= "<tr><td>".$row["Isi"]."</tr></td>";
+				}
+			}
+			?>
         <div class="grid-dosen-data">
             <div class="menu-bar-aspek">
                 <div class="button-menu" onclick="showPengajaranDosen()"><p>Pengajaran</p></div>
@@ -53,10 +70,39 @@
                 <div class="button-menu" onclick="showPenunjangDosen()"><p>Penunjang</p></div>
             </div>
             <div class="menu-bar-konten">
-                <div id="pengajaran-d" class="konten-data"><p>a</p></div>
-                <div id="penelitian-d" class="konten-data"><p>x</p></div>
-                <div id="pengabdian-d" class="konten-data"><p>c</p></div>
-                <div id="penunjang-d" class="konten-data"><p>s</p></div>
+                <div id="pengajaran-d" class="konten-data">
+					<p>
+					<table style="width:100%;">
+					<?php
+						echo $pengajaran;
+					?>
+					</table>
+					</p>
+				</div>
+                <div id="penelitian-d" class="konten-data">
+					<p>
+					<?php
+						echo $penelitian;
+					?>
+					</p>
+				</div>
+                <div id="pengabdian-d" class="konten-data">
+					<p>
+					<?php
+						echo $pengabdian;
+					?>
+					</p>
+				</div>
+                <div id="penunjang-d" class="konten-data">
+					<p>
+					<?php
+						echo $penunjang;
+					?>
+					</p>
+				</div>
+				<?php
+				
+				?>
             </div>
         </div>
     </div>
