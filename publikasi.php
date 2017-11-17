1effb383bf83eaabc722df4e0dd4a0ba40559e67
rionardians <rioriogue@gmail.com>
2017-11-17 14:14:45 +0700

    publikasi

---
+++ publikasi.php
@@ -0,0 +1,76 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Publikasi</title>    
+	<?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+    <div class="grid-container">
+        
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+		<div class="grid-headline-header">
+            <h1 class="headline-header"><span class="header-revcolor">PUBLIKASI</span></h1>
+        </div>
+        <div class="grid-headline-para">
+            <p class="headline-para">
+                Beikut adalah publikasi dari penelitan maupun seminar yang telah dilakukan oleh Dosen maupun mahasiswa Program Studi S1 FMIPA Universitas Padjadjaran.
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <img style="thumbnail" src="f_img/16.jpg" height="100%px" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2></h2>
+                <p>If you do not write for publication, there is little point in writing at all.
+					<br> <div style="text-align:right"> - George Bernard Shaw</div>
+				</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+				<h1>2016</h1>
+				<p>
+				<ol>
+					<li>Made Windu Antara Kesiman, Dona Valy, Jean-Christophe Burie, Erick Paulus, I Made Gede Sunarya, Setiawan Hadi, Kim Heng Sok, Jean-Marc Ogier. Asian Palm Leaf Manuscript Images: A New Challenge for Handwritten Text Line Segmentation Method. Published in Journal of Electronic Imaging (JEI) SPEI., 2016
+					</li>
+					<li>Harits Muhammad, R Sudrajat, Rudi Rosadi. Implementation of Data Mining in Analyzing Social Media Users Personality with Naive Bayes Classifier: A Case Study of Instagram Social Media, Published in IJCSI Journal, Volume 13, Issue 4, No 1, July 2016
+					</li>
+					<li>Evaluasi Aplikasi Semi-Immersive Virtual Reality pada Bidang Pendidikan Menurut Aspek Heuristik dan Pembelajaran, Jurnal Informatika dan Komputer (JIKO) STMIK AKAKOM, 2016
+					</li>
+					<li>Klasifikasi Newsgroup Menggunakan Vector Space Model dan K Nearest Neighbors, Jurnal Informatika UNPAD (JIN), 2016
+					</li>
+					<li>Pengaruh Ukuran Jendela Observasi Terhadap Kinerja Conditional Random Field pada Pengenalan Fase Gerak, Jurnal Informatika UNPAD (JIN), 2016
+					</li>
+					<li>Jean-Christophe Burie, Mickaël Coustaty, Setiawan Hadi, Made Windu Antara Kesiman, Jean-Marc Ogier, Erick Paulus, Kimheng Sok, I Made Gede Sunarya, Dona Valy. ICFHR2016 Competition on Analysis of Handwritten Text in Images of Balinese Palm Leaf Manuscripts. 15th Int. Conf. on Frontiers in Handwriting Recognition (ICFHR-2016) Proceedings 23-26, October 2016, Shanghai China
+					</li>
+					<li>Setiawan Hadi, Asep Sholahuddin, Lanny Rahmawati. The design and preliminary implementation of low-cost brain-computer interface for enable moving of rolling robot, International conference on Informatics and Computer (ICIC) Proceedings, 2016
+					</li>
+					<li>Mira Suryani, Ayi Muhammad Iqbal Nasuha, Setiawan Hadi. Sundanese Ancient Manuscript Retrieval System Comparison of Two Probability Approaches, International conference on Informatics and Computer (ICIC) Proceedings, 2016
+					</li>
+					<li>Ethnomathematics in Sundanese cultural perspective, Asian Mathematics Conference Poster, 2016
+					</li>
+					<li>Spatial data mining for evaluation standards of quality education in West Java using semiparametric geographical regression model, Asian Mathematics Conference Poster, 2016
+					</li>
+					<li>Data mining for classification naming of the village at Java island, Asian Mathematics Conference Proceedings, 2016
+					</li>
+					<li>Face recognition using moment invariant and neural network, Asian Mathematics Conference Poster, 2016
+					</li>
+				</ol>
+				</p>				
+				<h1>2015</h1>
+				<h1>2014</h1>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
