18afd31b87f1192d3ca993a5ee4b7bf03e556d65
rionardians <rioriogue@gmail.com>
2017-11-17 14:05:43 +0700

    pengabdian

---
+++ pengabdian.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Pengabdian</title>    
+	<?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+    <div class="grid-container">
+        
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+		<div class="grid-headline-header">
+            <h1 class="headline-header"><span class="header-revcolor">PENGABDIAN</span></h1>
+        </div>
+        <div class="grid-headline-para">
+            <p class="headline-para">
+                Pengabdian kepada masyarakat dilakukan sebagai sarana untuk mempererta hubungan lembaga dengan masyarakat sekitar sehingga eksistensi lembaga lebih baik dimata khalayak.
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <img style="thumbnail" src="f_img/15.jpg" height="100%px" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2></h2>
+                <p>We can't help everyone. But everyone can help someone.
+					<br> <div style="text-align:right"> - Ronald Raegan</div>
+				</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+				<h1>2016</h1>
+				<p>
+				<ul>
+					<li>PpM Fakultas MIPA
+						<ol>
+							<li style="margin-left:20px;">
+								Fun with Multimedia: SMP Muhamadiah Jatinangor.
+							</li>
+						</ol>
+					</li>
+				</ul>
+				</p>
+				
+				<h1>2015</h1>
+				<h1>2014</h1>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
