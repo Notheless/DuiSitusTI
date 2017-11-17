c8ecd57dec9d9e5085bedb4432dfb8e3464ceab1
rionardians <rioriogue@gmail.com>
2017-11-17 13:34:29 +0700

    sejarah

---
+++ sejarah.php
@@ -0,0 +1,59 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Sejarah</title>  
+	<?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+    <div class="grid-container">
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+		<div class="grid-headline-header">
+            <h1 class="headline-header"> <span class="header-revcolor"> SEJARAH</span></h1>
+        </div>
+        <div class="grid-headline-para">
+            <p class="headline-para">
+                Program Studi Teknik Informatika Universitas Padjadjaran merupakan salah satu Program Studi dalam Fakultas Matematika dan Ilmu Pengetahun Alam Universitas Padjadjaran.
+				<br><br>Pada awalnya Program Studi Teknik Informatika termasuk bagian dari Departemen Matematika.
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <span class="center-im"></span>
+            <img class="thumbnail" src="f_img/09	.jpg" height="auto" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2></h2>
+				<p><br>Gedung Dekanat Fakultas Matematika dan Ilmu Pengetahun Alam Universitas Padjadjaran.</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+                <h1>Pendidikkan D3</h1>
+                <p>
+                    Sebelum bedirinya pendidikan S1 untuk Prgram Studi Teknik Informatika FMIPA Universitas Padjadjaran, Program pendidikan D3 sudah berdiri dan sudah menghasilkan lulusan yang cukup berkualitas.
+                </p>
+                <h1>Pendiikan S1</h1>
+                <p>
+                    Program Studi Teknik Informatika (TI) FMIPA Unpad bertujuan menghasilkan lulusan yang mampu menyerap, mengaplikasikan, dan mengembangkan TI dengan kompetensi utama Rekayasa Perangkat Lunak, meliputi 4 peminatan studi yaitu Sistem Informasi, Jaringan Komputer dan Komunikasi Data, Sistem Cerdas dan Grafika Komputer, serta Komputasi dan Metode Numerik.
+                </p>
+                <p>
+                    Lulusan TI FMIPA Unpad diharapkan mampu mengembangkan perangkat lunak untuk berbagai keperluan, melakukan analisis terhadap sistem dalam suatu instansi, membuat solusi yang integratif dengan memanfaatkan perangkat lunak, mampu merencanakan dan mengevaluasi penerapan TI pada sebuah organisasi, mampu merancang dan memelihara basis data termasuk data warehouse untuk suatu instansi atau perusahaan, mampu melakukan pengujian fungsi-fungsi perangkat lunak, mampu mengembangkan, merancang dan membangun serta memelihara website dengan berbagai layanan dan fasilitasnya.
+                </p>
+                <h1>Prospek Kerja</h1>
+                <p>
+					Lulusan program studi TI FMIPA Unpad diarahkan dapat mengisi karir pekerjaan bidang supervisi, asisten manajer dan administrator. Sebagai contoh lulusan Teknik Informatika harus mampu menjadi Capacity Planning Supervisor, Database Administrator, Customer Service Supervisor, Data Communication Assistant manager, Data Entry Supervisor, Information Center Manager, Hardware Installation Supervisor, System Administrator, Webmaster, Project Manager Applications, Project Manager Distributed Systems, Project Network Technical Services, Project Manager Implementation Deployment, dan lain-lain.
+                </p>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
