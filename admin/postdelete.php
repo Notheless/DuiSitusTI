b1e93baf76cd2dca165344b47313f64bdd0ccc2b
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-11-19 11:31:11 +0700

    :a:

--- admin/postdelete.php
+++ admin/postdelete.php
@@ -23,7 +23,8 @@
 	}
 ?>
 <h2 class="content-title">Hapus Artikel</h2>
-                <form action="" method="get">
+                <form action="deletePE.php" method="POST">
+                    <input type="hidden" value="<?php echo $_GET["ID"] ?>" name="ID">
                     <table style="text-align:center">
                         <tr>
                             <td colspan="2"><p>Artikel dengan judul : </p></td>
@@ -35,9 +36,9 @@
                             <td colspan="2"><p>Akan dihapus dan tidak bisa dikembalikan, apakah anda yakin? </p></td>
                         </tr>
                         <tr>
-                            <td colspan="2"><input type="checkbox" style="display:inline-block; margin-top: 20px;"><p style="display:inline">Saya yakin dengan apa yang saya lakukan</p></td>
+                            <td colspan="2"><input type="checkbox" name="checkbox" style="display:inline-block; margin-top: 20px;"><p style="display:inline">Saya yakin dengan apa yang saya lakukan</p></td>
                         </tr>
                             <td><button type="button" onclick="hideInsertDiv()">kembali</button></td>
-                            <td style="text-align: right"><input class="button" type="submit" value="Hapus"></td>
+                            <td style="text-align: right"><input class="button" type="submit" value="Hapus" onclick="if(!this.form.checkbox.checked){alert('Silahkan centang Saya yakin dengan apa yang saya lakukan');return false}"></td>
                     </table>
                 </form>
