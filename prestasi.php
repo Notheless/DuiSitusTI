762247bdb09034e311ebdcbc88a61c0baceb3d41
rionardians <rioriogue@gmail.com>
2017-11-21 11:49:30 +0700

    prestasi

---
+++ prestasi.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Prestasi</title>
+    <?php include 'header.html';?>
+</head>
+<body>
+    <?php include 'Navbar.html';?>
+	<div class="grid-container">
+        <!-- navigasi kiri -->
+			<?php include 'left_nav.html';?>
+		<!--Recent-->
+			<?php include 'recent.html' ?>
+        <!-- konten -->
+        <div class="grid-headline-header">
+            <h1 class="headline-header"> <span class="header-revcolor"> PRESTASI </span></h1>
+        </div>
+        <div class="grid-headline-para">
+            <p class="headline-para">
+              Apakah Program Studi S1 Teknik Informatika Universitas Padjadjaran belum memiliki prestasi sama sekali?
+            </p>
+        </div>
+        <div class="grid-headline-thumbnail">
+            <img style="thumbnail" src="f_img/07.jpg" height="100%" width="100%"/>
+        </div>
+        <div class="grid-headline-thumbnail-det">
+            <div style="padding:10px;font-style:italic;color:white">
+                <h2>Laboratorium Komputer</h2>
+                <p>Merupakan tempat mahasiswa melaksanakan kegiatan praktikum maupun kegiatan belajar mengajar biasa.</p>
+            </div>
+        </div>
+        <div class="grid-content">
+            <div class="contentstyle" style="margin:40px;text-align:justify">
+                <!-- content start -->
+                <h1>Prestasi Mahasiswa</h1>
+                <h1>Prestasi Dosen</h1>
+                <!-- content end -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
