7865523cb5ce4295850957db8b1f57ca1df4dcda
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-12-06 15:20:41 +0700

    ignore~

--- database.php
+++ database.php
@@ -1,6 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
+$password = "meramera";
 $dbname = "situsti";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
