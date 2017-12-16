c477e7e33441d5c06ec8b5ea0a377fdabd1f8e12
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-12-16 15:42:23 +0700

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,9 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("lowernav").style.display = "none";
+        document.getElementById("logo").src = "img/logo_mini.png";
     } else {
         document.getElementById("lowernav").style.display = "block";
+        document.getElementById("logo").src = "img/logo_unpad.png";
     }
 }
