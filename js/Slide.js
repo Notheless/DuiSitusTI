63e43e492054d8fd6fb7e66c9f2815cd3a4a2e03
alfian21 <33252362+alfian21@users.noreply.github.com>
2017-12-17 23:54:52 +0700

    :clock1:

--- js/Slide.js
+++ js/Slide.js
@@ -23,4 +23,7 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+    setTimeout(function() {
+        plusSlides(1);
+    },2000);
 }
