3494a59647e091d01dbfd80404b3d082a83d1664
rionardians <rioriogue@gmail.com>
2017-12-18 11:42:52 +0700

    test gitignore

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "asasa";
 $dbname = "situsti";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
